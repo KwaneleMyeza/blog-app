387bca02936722ce394dc90335a36fe59250e040
Kwanele Myeza <gcinakahle.myeza@gmail.com>
2026-01-10 16:35:31 +0200

    Configure SQLAlchemy and update requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()  # create database instance
 
 def create_app():
     app = Flask(__name__)
+    basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'blog.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     from app.routes.main import main_bp
     app.register_blueprint(main_bp)
