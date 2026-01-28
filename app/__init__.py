88d83a4dfcacdf5264b1baf1e170c54738d5e1f0
Kwanele Myeza <gcinakahle.myeza@gmail.com>
2026-01-28 13:34:17 +0200

    Auto-create database tables on application startup

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,12 @@ def create_app():
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
 
+    # Create database tables automatically
+    with app.app_context():
+        db.create_all()
+
     return app
+
 
 @login_manager.user_loader
 def load_user(user_id):
